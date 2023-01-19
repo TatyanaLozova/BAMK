47d689e391145b0fa7bab95146926403f30a6afd
TatyanaLozova <68035704+TatyanaLozova@users.noreply.github.com>
2023-01-19 20:36:43 +0000

    +

---
+++ js/gallery-items.js
@@ -0,0 +1,88 @@
+export default [
+  {
+    preview:
+    './images/bamk/galery/1.jpg',
+    original:
+    './images/bamk/galery/1.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/2.jpg',
+    original:
+    './images/bamk/galery/2.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/3.jpg',
+    original:
+    './images/bamk/galery/3.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/4.jpg',
+    original:
+    './images/bamk/galery/4.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/5.jpg',
+    original:
+    './images/bamk/galery/5.jpg',
+    description: 'a',
+  },
+
+  {
+    preview:
+    './images/bamk/galery/6.jpg',
+    original:
+    './images/bamk/galery/6.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/7.jpg',
+    original:
+    './images/bamk/galery/7.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/8.jpg',
+    original:
+    './images/bamk/galery/8.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/9.jpg',
+    original:
+    './images/bamk/galery/9.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/10.jpg',
+    original:
+    './images/bamk/galery/10.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/11.jpg',
+    original:
+    './images/bamk/galery/11.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/12.jpg',
+    original:
+    './images/bamk/galery/12.jpg',
+    description: 'a',
+  },
+
+];
---
+++ js/gallery.js
@@ -0,0 +1,69 @@
+import images from "./gallery-items.js";
+
+
+
+const galleryContainer = document.querySelector('.js-gallery');
+const cardsGallery = containerGallery(images);
+galleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+ const openModalImage = document.querySelector('.js-lightbox');
+const modalImage = document.querySelector('.lightbox__image');
+// const boxOverlay = document.querySelector('.lightbox__overlay');
+
+const modalCloseBtn = document.querySelector('button[data-action="close-lightbox"]');
+ 
+galleryContainer.addEventListener('click', openModal);
+ modalCloseBtn.addEventListener('click', cloceModal);
+galleryContainer.addEventListener('click', onBackDropClick);
+// boxOverlay.addEventListener('click', overlay);
+
+
+// функция создания всей разметки
+function containerGallery(gallery) {
+  return gallery.map(({ original, preview, descriptiony }) => {
+    return`<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${descriptiony}"
+    />
+  </a>
+</li>
+    `
+  }).join('');
+
+}
+
+// функция для открытия модал
+function openModal(evt) {
+  evt.preventDefault()
+  if (!evt.target.classList.contains('gallery__image')) {
+    return
+  }
+  openModalImage.classList.add('is-open');
+  modalImage.src = evt.target.dataset.source
+}
+
+// функция для закрытия модал
+function cloceModal() {
+ 
+  openModalImage.classList.remove('is-open');
+// Очистка значения атрибута src элемента 
+modalImage.src = '';
+  }
+ 
+
+function onBackDropClick(event) {
+  if (event.currentTarget === event.target) {
+    closeModal()
+
+  }
+}
+
+
+
