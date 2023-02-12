8d1850d0a491e7fc991209ef937e1ca637ce334a
TatyanaLozova <68035704+TatyanaLozova@users.noreply.github.com>
2023-02-12 21:31:37 +0000

    +

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -84,5 +84,93 @@ export default [
     './images/bamk/galery/12.jpg',
     description: 'a',
   },
+  {
+    preview:
+    './images/bamk/galery/13.jpg',
+    original:
+    './images/bamk/galery/13.jpg',
+    description: 'a',
+    
+  },
+  {
+    preview:
+    './images/bamk/galery/14.jpg',
+    original:
+    './images/bamk/galery/14.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/15.jpg',
+    original:
+    './images/bamk/galery/15.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/9.jpeg',
+    original:
+    './images/bamk/galery/9.jpeg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/16.jpg',
+    original:
+    './images/bamk/galery/16.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/17.jpg',
+    original:
+    './images/bamk/galery/17.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/18.jpg',
+    original:
+    './images/bamk/galery/18.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/19.jpg',
+    original:
+    './images/bamk/galery/19.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/20.jpg',
+    original:
+    './images/bamk/galery/20.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/21.jpg',
+    original:
+    './images/bamk/galery/21.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/22.jpg',
+    original:
+    './images/bamk/galery/22.jpg',
+    description: 'a',
+  },
+  {
+    preview:
+    './images/bamk/galery/23.jpg',
+    original:
+    './images/bamk/galery/23.jpg',
+    description: 'a',
+  },
+
+
+
 
 ];
